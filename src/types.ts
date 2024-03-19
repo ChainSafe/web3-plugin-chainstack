7a9cdbafbc9123cd089282b648ee8970017d6d30
jdevcs <junaid@chainsafe.io>
2024-03-19 18:56:51 +0100

    types updates

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,7 @@ export type Creator = {
   first_name: string;
   id: string;
   last_name: string;
-  organization: {
-    id: string;
-    name: string;
-  };
+  organization: Organization;
 };
 
 export type Organization = {
@@ -19,35 +16,24 @@ export type Project = {
   name: string;
   description: string;
   type: string;
+  members: number;
   networks: number;
   creator: Creator;
   created_at: string;
 };
+
 export type ProjectsResponse = {
   count: number;
-  next: null; // TODO: Check the case when next is not null
-  previous: null; // TODO: Check the case when previous is not null
+  next: string; // TODO: Check the case when next is not null
+  previous: string; // TODO: Check the case when previous is not null
   results: Project[];
 };
 
 export type RpcNode = {
-  configuration: {
-    archive: boolean;
-    client: string;
-  };
+  configuration: NodeConfiguration;
   created_at: string;
   creator: Creator;
-  details: {
-    api_namespaces: string[];
-    auth_key: string;
-    auth_password: string;
-    auth_username: string;
-    beacon_endpoint: string;
-    https_endpoint: string;
-    mode: string;
-    version: string;
-    wss_endpoint: string;
-  };
+  details: NodeDetails;
   id: string;
   name: string;
   network: string;
@@ -58,42 +44,73 @@ export type RpcNode = {
   status: string;
   type: string;
 };
+
+export type NodeConfiguration = {
+  archive: boolean;
+  client: string;
+};
+
+export type NodeDetails = {
+  api_namespaces: string[];
+  auth_key: string;
+  auth_password: string;
+  auth_username: string;
+  beacon_endpoint: string;
+  https_endpoint: string;
+  mode: string;
+  version: string;
+  wss_endpoint: string;
+};
+
 export type RpcNodesResponse = {
   count: number;
-  next: null; // TODO: Check the case when next is not null
-  previous: null; // TODO: Check the case when previous is not null
+  next: string; // TODO: Check the case when next is not null
+  previous: string; // TODO: Check the case when previous is not null
   results: RpcNode[];
 };
 
 export type Network = {
+  configuration: {};
   id: string;
   name: string;
   email: string;
-  createdAt: string;
-  updatedAt: string;
+  created_at: string;
+  updated_at: string;
+
+  project?: string;
+  protocol?: string;
+  details?: {};
+  status?: string;
+  creator?: Creator;
+  nodes?: Node[];
 };
+
 export type NetworksResponse = {
   count: number;
-  next: null; // TODO: Check the case when next is not null
-  previous: null; // TODO: Check the case when previous is not null
+  next: string; // TODO: Check the case when next is not null
+  previous: string; // TODO: Check the case when previous is not null
+  results: Networks[];
+};
 
-  results: Array<{
-    configuration: {
-      network: string;
-    };
-    created_at: string;
-    creator: Creator;
-    details: {};
-    id: string;
-    name: string;
-    nodes: RpcNode[];
-    project: string;
-    protocol: string;
-    status: string;
-  }>;
+export type Networks = {
+  created_at: string;
+  creator: Creator;
+  details: {};
+  id: string;
+  name: string;
+  nodes: RpcNode[];
+  project: string;
+  protocol: string;
+  status: string;
+  configuration: {
+    network: string;
+  };
 };
 
 export type Identity = {
+  email: string;
+  created_at: string;
+  updated_at: string;
   id: string;
   name: string;
   protocol: string;
@@ -103,7 +120,7 @@ export type Identity = {
 
 export type IdentitiesResponse = {
   count: number;
-  next: null; // TODO: Check the case when next is not null
-  previous: null; // TODO: Check the case when previous is not null
+  next: string; // TODO: Check the case when next is not null
+  previous: string; // TODO: Check the case when previous is not null
   results: Identity[];
 };
